cc44c33745d6c9f4acb73fd468c5f510159cd36c
DOUGR4S <141284830+DOUGR4S@users.noreply.github.com>
2023-11-24 21:26:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res)=>{
 app.post("/", (req, res)=>{
     const {nome, email, telefone, checkin, checkout} = req.body;
     insertReserva(nome, email, telefone, checkin, checkout);
-    res.send("Obrigado pela reserva, " + nome)
+    res.sendFile('public/index.html', { root: __dirname});
 });
 
 //TIJb9gBpzYZ0RZ2q (senha MongoDB)
